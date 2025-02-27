73c426e8cc5f291bacdb99189a222cb8bc108553
iliyas <iliyas.yussupov@gmail.com>
2025-02-27 14:09:53 +0100

    init commit

---
+++ src/main/java/org/example/Scoreboard.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Scoreboard {
+
+
+    public void startMatch(){
+
+    }
+
+    public void finishMatch(){
+
+    }
+
+    public void updateScore(){
+
+    }
+
+    public List<Match> getSummary(){
+
+        return new ArrayList<>();
+    }
+}
